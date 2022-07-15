87fa5c39a44fa41dcbca5950aa77ecd0d209f728
Brenda Liu <brenda.liu24@gmail.com>
2022-07-15 15:29:22 -0700

    complete average and construct word functions

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,34 @@
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+
+/** determines if there are a pair of numbers whose avg is the target value */
+
+
+function averagePair(nums, targetAvg) {
+  //make left pointer
+  let left = 0;
+  //make right pointer
+  let right = nums.length - 1;
+
+  //make while left is less than right loop
+  while (left < right){
+    //find average of left and right
+    let average = (nums[left] + nums[right])/2;
+      //if average is target
+      if (average === targetAvg){
+        //return true
+        return true;
+      //if average is greater than target
+      } else if (average > targetAvg){
+        //move right pointer left
+        right--;
+      //else
+      } else {
+        //move left pointer right
+        left++;
+      }
+  }
+
+  //return false
+  return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,36 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+
+/** returns true if word can be build with letters, otherwise return false*/
+
+function canConstructWord(word,letters) {
+  if (!letters.length) return false;
+  //make freq for word
+  const wordFreq = frequencyMaker(word);
+  //make freq for letters
+  const lettersFreq = frequencyMaker(letters);
+
+  //loop through word freq
+  for(let key in wordFreq){
+    //if key: value is <= key: value letter freq
+    //if not return false
+    if(wordFreq[key] > lettersFreq[key]) return false;
+  }
+  //return true
+  return true;
+}
+
+function frequencyMaker (string) {
+  //make freq
+  const freq = {};
+
+  //loop through string
+  for (let letter of string){
+  //add letter if it doesn't exist
+  //increment if it already in freq
+    freq[letter] = (freq[letter] || 0) + 1;
+  }
+
+  //return freq
+  return freq;
+
 }
