15769ce35eb4726f9b08396591486a06ed37fb61
Raytene Han <raytenehan@gmail.com>
2022-07-15 15:38:31 -0700

    completed two-array-object function

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+
+  // create empty object that takes the first array as keys and the second array as values
+  const obj = {};
+  // loop over the first array and assigns the element in the first array as the key in the object
+  for (let i = 0; i < keys.length; i++) {
+  // and the element in second array as the key
+  // if the element in second array does not exist, the value becomes null
+    if (values[i] === undefined) {
+      obj[keys[i]] = null;
+    } else {
+      obj[keys[i]] = values[i];
+    }
+  }
+  return obj;
 }
