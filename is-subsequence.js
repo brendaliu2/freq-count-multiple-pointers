2daca5b9c8552dc14d7f27e568f3736844366ee6
Brenda Liu <brenda.liu24@gmail.com>
2022-07-15 17:00:34 -0700

    solve subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,26 @@
+'use strict';
 // add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+
+/** takes 2 strings and checks if first string is part of second string,
+ * while maintaining same order of letters
+ */
+
+//pointer for s1
+//looping through s2
+//if letters of both are same, increment s1 pointer
+//if s1 point is s1 length -1 , return true
+
+function isSubsequence(s1, s2) {
+  if (s2.length < s1.length) return false;
+
+  let s1Pointer = 0;
+  let isSubstring = false;
+  for (let letter of s2) {
+    debugger;
+
+    if (letter === s1[s1Pointer]) s1Pointer++;
+    if (s1Pointer === s1.length) return true;
+  }
+
+  return false;
 }
